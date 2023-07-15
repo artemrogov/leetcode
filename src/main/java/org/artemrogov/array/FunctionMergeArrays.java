ba5c859695e6f5e9e6846943d749db551f631180
artem rogov <artemrogov281@gmail.com>
2023-07-16 01:38:31 +0300

    merge two arrays

---
+++ src/main/java/org/artemrogov/array/FunctionMergeArrays.java
@@ -0,0 +1,7 @@
+package org.artemrogov.array;
+
+
+@FunctionalInterface
+public interface FunctionMergeArrays {
+    void merge(int[] nums1, int m, int[] nums2, int n);
+}
--- src/main/java/org/artemrogov/array/SolutionMergeTwoArray.java
+++ src/main/java/org/artemrogov/array/SolutionMergeTwoArray.java
@@ -1,24 +1,23 @@
 package org.artemrogov.array;
 
-class Solution {
-    public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m - 1;
-        int j = n - 1;
+import java.util.Arrays;
 
-        int lastIndex = m + n - 1;
-        while (j >= 0){
-            if (i >= 0 && nums1[i] > nums2[j]){
-                nums1[lastIndex] = nums1[i];
-                i--;
-            } else{
-                nums1[lastIndex] = nums2[j];
-                j--;
-            }
-            lastIndex--;
-        }
-    }
-}
 public class SolutionMergeTwoArray {
     public static void main(String[] args) {
+        FunctionMergeArrays mergeArrays = (nums1, m, nums2, n) -> {
+            int resultArraySize = m + n;
+            int[] resultArray = new int[resultArraySize];
+            System.arraycopy(nums1,0,resultArray,0,m);
+            System.arraycopy(nums2,0,resultArray,m,n);
+            System.out.println(Arrays.toString(resultArray));
+        };
+
+        int[] array01 = new int[]{1,2,3,4,5};
+        int[] array02 = new int[]{6,7,8,9,10};
+
+        int lengthArray01 = array01.length;
+        int lengthArray02 = array02.length;
+
+        mergeArrays.merge(array01,lengthArray01,array02,lengthArray02);
     }
 }
